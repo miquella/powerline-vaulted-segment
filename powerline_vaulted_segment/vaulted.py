086aabacd025f427fd5773dc7c2f876ca80cd662
Mark Severson <miquella@gmail.com>
2015-10-23 12:24:13 -0600

    Basic segment functionality

---
+++ powerline_vaulted_segment/vaulted.py
@@ -0,0 +1,15 @@
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+from powerline.theme import requires_segment_info
+
+
+@requires_segment_info
+def vaulted(pl, segment_info, prefix=None):
+    '''Return the current vaulted vault
+
+    :param string prefix:
+        The prefix to use in front of the vault name
+    '''
+    vault = segment_info['environ'].get('VAULTED_ENV', None)
+    if vault:
+        return '{0}{1}'.format(prefix, vault)
