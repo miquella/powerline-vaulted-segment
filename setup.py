8740360b81d6c6855ded6dc37c80e546bbc77c27
Mark Severson <miquella@gmail.com>
2015-12-02 13:53:02 -0700

    fix namespacing issue with setuptools

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup, find_packages
 setup(
         name='powerline-vaulted-segment',
         packages=find_packages(),
-        namespace_packages=['powerline_vaulted_segment'],
         version='0.1.0'
         )
